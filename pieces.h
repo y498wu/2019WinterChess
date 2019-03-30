7d47fbf11bc233fc31ee111debc4f28a44e0d9cc
eOlaru <49070040+eOlaru@users.noreply.github.com>
2019-03-30 13:49:11 -0400

    Add files via upload

--- pieces.h
+++ pieces.h
@@ -4,22 +4,28 @@
 #include "pieces.cc"
 #include "position.h"
 #include <vector>
+#include <string>
 
 class Pieces{
 		
 	Board *theBoard;
-	bool isWhite;
+	bool White;
 	Position Location;
 	vector<Position> LegalMoves;
+	bool Protected;
+	Piece *pinned;
 	
 public: 
 	
 	//non virtual methods, these will behave the same for every piece
 	bool canMove(Position target) const;
 	Position getPos() const;
+	bool isWhite() const;
 	void setPos(Position newPos);
 	
+	
 	//virtual methods, these need to be defined in each piece
+	virtual std::string checkType() const = 0;
 	virtual void updateMoves() = 0;	
 	virtual ~Pieces();
  
