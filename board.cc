2f04f6e01ba94d6f4b724267345aa5fedc5bb323
y498wu <49050856+y498wu@users.noreply.github.com>
2019-03-31 20:20:52 -0400

    add some setup methods

--- board.cc
+++ board.cc
@@ -21,59 +21,127 @@
 using namespace std;
 
 Board::Board(int WIDTH, int HEIGHT) : WIDTH{8}, HEIGHT{8},
-hasSetup{false}, hasPlay{false}, whiteScore{0}, blackScore{0}, textOrGraphic{true},
+hasSetup{false}, hasPlay{false}, whiteScore{0}, blackScore{0}, 
+textOrGraphic{true}, isWhiteTurn{true},
 {
-	for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
+	for(int i = 0; i < HEIGHT; i++){
+		for(int j = 0; j < WIDTH; j++){
 			pieces[i][j] = nullptr;
 		}
 	}
 }
 
+// get the Piece* at pos
 Piece* Board::atLocation(Position pos){
 	return pieces[pos.getY][pos.getX];
 }
 
-void Board::addPiece(string pieceType, Position pos){
+// start setup mode
+void Board::setupStart(){
+	// if the play has started without setup, throw an error
+	if(hasPlay){
+		throw("The game starts without being set up!");
+	}
+	hasSetup = true;
+}
+
+// add a piece during setup stage
+void Board::addPieceSetup(string pieceType, Position pos){
+	// check if the setup stage has started
+	if (!hasSetup){
+		throw("The setup stage has not started! Call setupStart method!");
+	}
+
 	bool isWhite;
-	// check if the piece is white or blackß
+	// check if the piece is white or black
 	if (pieceType[0] <= 'Z'){
 		isWhite = true;
 	} else {
 		isWhite = false;
 	}
 
-    // if there is already a piece at pos, throw an error
+    // if there is already a piece at pos, free this owner
 	if(atLocation(pos)){
-		throw("There is already a piece at this position!");
+		delete pieces[pos.getY][pos.getX];
 	}
-
-	// free the owner piece at pos
-	delete pieces[pos.getY][pos.getX];
 
 	// contruct a new piece pointer to the position on board
 	if (pieceType == "K" || pieceType == "k"){
 		// need to wait Eric for the constructor of King/Queen/...
-		atLocation(pos) = new King(isWhite, pos);
+		atLocation(pos) = new King(this, isWhite, pos);
 	} else if (pieceType == "Q" || pieceType == "q"){
-		atLocation(pos) = new Queen(isWhite, pos);
+		atLocation(pos) = new Queen(this, isWhite, pos);
 	} else if (pieceType == "R" || pieceType == "r"){
-		atLocation(pos) = new Rook(isWhite, pos);
+		atLocation(pos) = new Rook(this, isWhite, pos);
 	} else if (pieceType == "H" || pieceType == "h"){
-		atLocation(pos) = new Knight(isWhite, pos);
+		atLocation(pos) = new Knight(this, isWhite, pos);
 	} else if (pieceType == "B" || pieceType == "b"){
-		atLocation(pos) = new Bishop(isWhite, pos);
+		atLocation(pos) = new Bishop(this, isWhite, pos);
 	} else if (pieceType == "P" || pieceType == "p"){
-		atLocation(pos) = new Pawn(isWhite, pos);
+		atLocation(pos) = new Pawn(this, isWhite, pos);
 	}
 }
 
+// remove a piece during the setup stage
+void Board::removePieceSetup(Position pos){
+	// check if the setup stage has started
+	if (!hasSetup){
+		throw("The setup stage has not started! Call setupStart method!");
+	}
+
+	if(atLocation(pos)){
+		delete pieces[pos.getY][pos.getX];
+	}
+}
+
+// determine whose turn to go next
+void colourSetup(bool isWhite){
+	// check if the setup stage has started
+	if (!hasSetup){
+		throw("The setup stage has not started! Call setupStart method!");
+	}
+
+	if(isWhite){
+		isWhiteTurn = true;
+	} else {
+		isWhiteTurn = false;
+	}
+}
+
+void originalSetup(){
+	// set the black side at row #0
+	pieces[0][0] = new Rook(this, false, Position(0, 0));
+	pieces[0][1] = new Knight(this, false, Position(1, 0));
+	pieces[0][2] = new Bishop(this, false, Position(2, 0));
+	pieces[0][3] = new Queen(this, false, Position(3, 0));
+	pieces[0][4] = new King(this, false, Position(4, 0));
+	pieces[0][5] = new Bishop(this, false, Position(5, 0));
+	pieces[0][6] = new Knight(this, false, Position(6, 0));
+	pieces[0][7] = new Rook(this, false, Position(7, 0));
+	// set the black side at row #1
+	for(int i = 0; i < 7; ++i){
+		pieces[1][i] = new Pawn(this, false, Position(i, 1));
+	}
+	// set the white side at row #6
+    for(int j = 0; i < 7; ++i){
+		pieces[6][j] = new Pawn(this, true, Position(j, 6));
+	}
+	// set the white side at row #7
+	pieces[7][0] = new Rook(this, true, Position(0, 7));
+	pieces[7][1] = new Knight(this, true, Position(1, 7));
+	pieces[7][2] = new Bishop(this, true, Position(2, 7));
+	pieces[7][3] = new Queen(this, true, Position(3, 7));
+	pieces[7][4] = new King(this, true, Position(4, 7));
+	pieces[7][5] = new Bishop(this, true, Position(5, 7));
+	pieces[7][6] = new Knight(this, true, Position(6, 7));
+	pieces[7][7] = new Rook(this, true, Position(7, 7));
+}
 
 // makeMove method needs to consider other cases
 void Board::makeMove(Position start, Position end, string pieceType){
 	// if there is already a piece at Position end, throw an error
-	if(atLocation(end)){
-		throw("There is already a piece at Position end!");
+	if(!atLocation(start)){
+		throw("There is no piece at Position start!");
 	}
 
 	// move the piece pointer at Position start to Position end
