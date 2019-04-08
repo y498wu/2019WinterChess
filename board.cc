3dc94173792c718b06961201b33280e94cc5afc0
eOlaru <49070040+eOlaru@users.noreply.github.com>
2019-04-08 17:53:42 -0400

    Add files via upload

--- board.cc
+++ board.cc
@@ -77,9 +77,10 @@ void Board::updateBoard(){
 		}	
 	}
 	//reset the check, so that it can be recalculated
-	whiteKing->putInCheck(nullptr);
-	blackKing->putInCheck(nullptr);
-	
+	if(whiteKing != nullptr && blackKing != nullptr ){
+		whiteKing->putInCheck(nullptr);
+		blackKing->putInCheck(nullptr);
+	}
 	// go through all the Piece* to update their legalMoves
 	for(int y = 0; y <= 7; ++y){
 		for(int x = 0; x <= 7; ++x){
